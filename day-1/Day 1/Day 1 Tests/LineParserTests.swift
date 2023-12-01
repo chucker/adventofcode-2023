61ca8dc48d45cffd85d00fb33ef4fbbf46978650
Sören Kuklau <kuklau@trenz.de>
2023-12-01 15:06:13 +0100

    day 1, part 2, in Swift

--- day-1/Day 1/Day 1 Tests/LineParserTests.swift
+++ day-1/Day 1/Day 1 Tests/LineParserTests.swift
@@ -8,13 +8,29 @@
 import XCTest
 
 final class LineParserTests: XCTestCase {
-    func testExample() throws {
-        let testCases = [("1abc2", "1", "2", 12),
-                         ("pqr3stu8vwx", "3", "8", 38),
-                         ("a1b2c3d4e5f", "1", "5", 15),
-                         ("treb7uchet", "7", "7", 77)]
-
-        for testCase in testCases {
+    let inputsPart1 = [("1abc2", "1", "2", 12),
+                       ("pqr3stu8vwx", "3", "8", 38),
+                       ("a1b2c3d4e5f", "1", "5", 15),
+                       ("treb7uchet", "7", "7", 77)]
+    
+    let inputsPart2 = [("two1nine", "2", "9", 29),
+                       ("eightwothree", "8", "3", 83),
+                       ("abcone2threexyz", "1", "3", 13),
+                       ("xtwone3four", "2", "4", 24),
+                       ("4nineeightseven2", "4", "2", 42),
+                       ("zoneight234", "1", "4", 14),
+                       ("7pqrstsixteen", "7", "6", 76)]
+    
+    func testPart1() throws {
+        try test(testCase: inputsPart1)
+    }
+    
+    func testPart2() throws {
+        try test(testCase: inputsPart2)
+    }
+    
+    func test(testCase: [(String, String, String, Int)]) throws {
+        for testCase in testCase {
             let input = testCase.0
 
             let parser = LineParser(line: input)
--- day-1/Day 1/Day 1/LineParser.swift
+++ day-1/Day 1/Day 1/LineParser.swift
@@ -14,30 +14,89 @@ struct LineParser {
 
     let line: String
 
-     func getFirstAndLastDigit() -> (String, String) {
-        var firstDigit: String?
-        var lastDigit: String?
+    func getFirstAndLastDigit() -> (String, String) {
+        var indices = [String.Index: String]()
 
-        for c in line {
-            if c.isNumber {
-                firstDigit = String(c)
-                break
+        for num in ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] {
+            for index in line.indices(of: num) {
+                indices[index] = LineParser.parseNum(num: num)!
             }
         }
 
-        for c in line.reversed() {
-            if c.isNumber {
-                lastDigit = String(c)
-                break
+        for index in 0 ..< line.count {
+            let stringIndex = String.Index(utf16Offset: index, in: line)
+
+            let char = line[stringIndex]
+
+            if char.isNumber {
+                indices[stringIndex] = String(char.wholeNumberValue!)
             }
         }
+
+        let sortedKeys = indices.keys.sorted(by: <)
+
+        let firstDigit = indices[sortedKeys.first!]
+        let lastDigit = indices[sortedKeys.last!]
 
         return (firstDigit!, lastDigit!)
     }
-    
+
+    static func parseNum(num: String) -> String? {
+        switch num {
+        case "one":
+            return "1"
+        case "two":
+            return "2"
+        case "three":
+            return "3"
+        case "four":
+            return "4"
+        case "five":
+            return "5"
+        case "six":
+            return "6"
+        case "seven":
+            return "7"
+        case "eight":
+            return "8"
+        case "nine":
+            return "9"
+        default:
+            return nil
+        }
+    }
+
     func getCalibrationValue() -> Int {
         let digits = getFirstAndLastDigit()
-        
+
         return Int(digits.0 + digits.1)!
+    }
+}
+
+extension StringProtocol {
+    func index<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+        range(of: string, options: options)?.lowerBound
+    }
+
+    func endIndex<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+        range(of: string, options: options)?.upperBound
+    }
+
+    func indices<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Index] {
+        ranges(of: string, options: options).map(\.lowerBound)
+    }
+
+    func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>] {
+        var result: [Range<Index>] = []
+        var startIndex = self.startIndex
+        while startIndex < endIndex,
+              let range = self[startIndex...]
+              .range(of: string, options: options)
+        {
+            result.append(range)
+            startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+                index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+        }
+        return result
     }
 }
